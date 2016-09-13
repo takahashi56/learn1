9dddbad7523b1c28736f2dd00a9f50d34c871469
frans870503 <frans870503@yandex.com>
2016-09-13 09:12:24 -0400

    update second 9.12

--- app/components/admin/add/content.ts
+++ app/components/admin/add/content.ts
@@ -38,14 +38,14 @@ export class LessonContent implements OnInit{
 
 		this.videoOrQuestion = this.content.videoOrQuestion;
 		this.singleOrMulti = this.content.singleOrMulti;
-		this.videoLabel = new Control(this.content.videoLabel,Validators.required); 
+		this.videoLabel = new Control(this.content.videoLabel,Validators.required);
 		this.videoEmbedCode = new Control(this.content.videoEmbedCode , Validators.required);
 		this.question = new Control(this.content.question, Validators.required);
 		this.answerA = new Control(this.content.answerA , Validators.required);
 		this.answerB = new Control(this.content.answerB , Validators.required);
 		this.answerC = new Control(this.content.answerC , Validators.required);
 		this.trueNumber = this.content.trueNumber;
-		
+
 		this.contentForm = this.builder.group({
 			videoLabel : this.videoLabel,
 			videoEmbedCode : this.videoEmbedCode,
@@ -60,8 +60,9 @@ export class LessonContent implements OnInit{
 	choiceChange(flag: boolean){
 		this.singleOrMulti = flag;
 	}
-	choiceAnswer(n: number){
+	choiceAnswer(form: any, n: number){
 		this.trueNumber = n;
+		this.blurChange(form);
 	}
 
 	removeContent(){
@@ -75,7 +76,7 @@ export class LessonContent implements OnInit{
 		original = original.filter(function( obj ) {
 			var content_id = obj._content_id ? obj._content_id : obj._id;
 
-			return content_id !== id;	
+			return content_id !== id;
 		});
 		this._session.setItem('Content', JSON.stringify(original));
 		this.manageContent.emit(original);
@@ -92,7 +93,7 @@ export class LessonContent implements OnInit{
 
 			switch (this.singleOrMulti) {
 				case false:
-					if(form.question != "" ) updated = false; 
+					if(form.question != "" ) updated = false;
 					break;
 				case true:
 					if(form.question == "" || form.answerA == "" || form.answerB == "" || form.answerC == "") {
@@ -102,24 +103,24 @@ export class LessonContent implements OnInit{
 						console.log("false");
 						updated = false;
 					}
-					break;				
+					break;
 				default:
-					updated = true;					
+					updated = true;
 					break;
 			}
 		}
 
 		console.log(form);
-		
+
 		if(!updated){
 			this.content.videoLabel = form.videoLabel;
-			this.content.videoEmbedCode = form.videoEmbedCode;	
+			this.content.videoEmbedCode = form.videoEmbedCode;
 			this.content.question = form.question;
-			this.content.singleOrMulti = this.singleOrMulti;			
+			this.content.singleOrMulti = this.singleOrMulti;
 			this.content.answerA = form.answerA;
 			this.content.answerB = form.answerB;
 			this.content.answerC = form.answerC;
-			this.content.trueNumber = this.trueNumber;		
+			this.content.trueNumber = this.trueNumber;
 
 			var data = this.content;
 			original.forEach(function(obj){
@@ -127,7 +128,7 @@ export class LessonContent implements OnInit{
 				var second_id = data._content_id ? data._content_id: data._id;
 
 				console.log(obj);
-				if((first_id == second_id)){			
+				if((first_id == second_id)){
 					original_copy.push(data)
 				}else{
 					original_copy.push(obj)
@@ -148,34 +149,34 @@ export class LessonContent implements OnInit{
 
 			switch (this.singleOrMulti) {
 				case false:
-					if(form.question != "" ) updated = false; 
+					if(form.question != "" ) updated = false;
 					break;
 				case true:
-					if(form.question == "" || form.answerA == "" || form.answerB == "" || form.answerC == "" || this.trueNumber == 0 ) {
+					if(form.question == "" || form.answerA == "" || form.answerB == "" || form.answerC == "" ) {
 						console.log("true");
 						updated = true;
 					}else{
 						console.log("false");
 						updated = false;
 					}
-					break;				
+					break;
 				default:
-					updated = true;					
+					updated = true;
 					break;
 			}
 		}
 
 		console.log(form);
-		
+
 		if(!updated){
 			this.content.videoLabel = form.videoLabel;
-			this.content.videoEmbedCode = form.videoEmbedCode;	
+			this.content.videoEmbedCode = form.videoEmbedCode;
 			this.content.question = form.question;
-			this.content.singleOrMulti = this.singleOrMulti;			
+			this.content.singleOrMulti = this.singleOrMulti;
 			this.content.answerA = form.answerA;
 			this.content.answerB = form.answerB;
 			this.content.answerC = form.answerC;
-			this.content.trueNumber = this.trueNumber;		
+			this.content.trueNumber = this.trueNumber;
 			var data = this.content;
 
 			var new_data = {
