172cd4a0ac451b16eb1e9388c818687242b8a229
frans870503 <frans870503@yandex.com>
2016-09-14 11:48:27 -0400

    complete date on course

--- app/components/tutor/detail/course.ts
+++ app/components/tutor/detail/course.ts
@@ -18,7 +18,7 @@ export class DetailTutorCourse {
 
 	constructor(private _session: Session, private _tutorService: TutorService,  private _router: Router) {
 		this.course = JSON.parse(this._session.getItem('TutorCourse'));
-		
+
 		this._tutorService.getStudentsByCourseId(this.course.course_id, this._session.getCurrentId()).subscribe((res)=>{
 			this.studentList = res;
 		})
@@ -41,8 +41,15 @@ export class DetailTutorCourse {
 	}
 
 	private titleCase(str: string) : string {
-	 	return str.split(' ').map(function(val){ 
+	 	return str.split(' ').map(function(val){
 	    	return val.charAt(0).toUpperCase() + val.substr(1).toLowerCase();
 	  	}).join(' ');
+	}
+
+	getCompleteDate(date){
+		if(date == null) return '';
+		var d = new Date(date),
+				datestring = d.getDate()  + "/" + (d.getMonth()+1) + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+		return datestring;
 	}
 }
