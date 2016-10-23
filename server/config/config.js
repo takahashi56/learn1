d79cdc5e775b7738a4634297de81ddc55d77d304
frans870503 <frans870503@yandex.com>
2016-10-23 17:55:42 -0400

    update

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,8 @@ module.exports = {
 	},
 	production: {
 		db: 'mongodb://5woody:gb88200gtx@ds029575.mlab.com:29575/learningdb',
+		auth0_secret: 'fLazuOMz74H5aO4Wb4G5tXnREGvrkCUwSf0JHZ6IzDHZhHRMUMG6cAukHtS1ivVG',
+		auth0_client_id: 'dnrHEOwxpNGG3CyCxB9GABXvIPIlFHXp',
 		postmark_api: "17c11810-aceb-45b6-8495-35dad3962dde",
 		stripe_secret_key:"sk_test_kDMCSHs3rjNp8L2v7ay8JptV",
 		stripe_publish_key: "pk_test_We45bLO76vdmkYnW7Lr5KIpa",
