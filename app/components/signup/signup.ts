671e8610f6a900f7df05d7f9199139b79a826a6b
frans870503 <frans870503@yandex.com>
2016-10-25 11:57:38 -0400

    update phone in sign up

--- app/components/signup/signup.ts
+++ app/components/signup/signup.ts
@@ -43,6 +43,14 @@ export class SignUp {
 		})
 	}
 
+	isNumberKey(evt) {
+			evt = (evt) ? evt : window.event;
+			var charCode = (evt.which) ? evt.which : evt.keyCode;
+			if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+					return false;
+			}
+			return true;
+	}
 
 	AddOrgs(form: any) {
 		this.registerFailure = false;
