63a7347b53dcc1d6e66ee376ff814bbd008858d3
pedro19880417 <pedro19880417@yahoo.com>
2016-11-28 13:19:38 -0500

    update

--- app/components/matrix/matrix.ts
+++ app/components/matrix/matrix.ts
@@ -46,31 +46,34 @@ export class Matrix  implements AfterViewInit {
 
 	downloadpdf(){
 			let self = this;
-			html2canvas(document.getElementById('pdffromHtml'), {
-					onrendered: function (canvas) {
-							var ctx = canvas.getContext("2d");
-							var data = canvas.toDataURL("image/png", 1.5);
-							console.log(data)
-							var docDefinition = {
-									content: [{
-											image: data,
-											width: 520,
-									}]
-							};
+			document.getElementById('gridbasic').parentNode.style.overflow = 'visible';
+			html2canvas( [ document.getElementById('gridbasic') ], {
+			 onrendered: function(canvas) {
+					 document.getElementById('gridbasic').parentNode.style.overflow = 'hidden';
+					 var ctx = canvas.getContext("2d");
+					 var data = canvas.toDataURL("image/png", 1.5);
+					 console.log(data)
+					 var docDefinition = {
+							 content: [{
+									 image: data,
+									 width: 520,
+							 }]
+					 };
 
-							var para =  document.getElementById("editor"),
-									para1 = document.createElement("div"),
-									image = document.createElement("img"),
-									image1 = document.createElement('img');
-							image.src = data;
-							para.appendChild(image);
-							// self.makeHighResScreenshot(image, image1, 200);
-							// para1.appendChild(image);
-							self._tutorService.makePdf({data:para.innerHTML, direction: false}).subscribe((res) => {
-								 window.location.href = '/pdf-viewer/web/viewer.html?file=/pdf/' + res.url;
-							})
-					}
-			});
+					 var para =  document.getElementById("editor"),
+							 para1 = document.createElement("div"),
+							 image = document.createElement("img"),
+							 image1 = document.createElement('img');
+					 image.src = data;
+					 para.appendChild(image);
+					 // self.makeHighResScreenshot(image, image1, 200);
+					 // para1.appendChild(image);
+					 self._tutorService.makePdf({data:para.innerHTML, direction: false}).subscribe((res) => {
+							window.location.href = '/pdf-viewer/web/viewer.html?file=/pdf/' + res.url;
+					 })
+
+			 }
+		});
 	}
 
   getCompleteDatefromCourse(course, student_course){
