c90985221077457b2c4dba6a95417ba5a0bab27d
chrisroad <chrisbrownapple001@gmail.com>
2016-12-16 10:20:31 -0500

    update

--- app/components/student/video/student.ts
+++ app/components/student/video/student.ts
@@ -5,56 +5,67 @@ import {CanActivate} from 'angular2/router';
 import {StudentService} from '../../../services/student';
 
 @Component({
-	selector: 'student-video',
-	templateUrl: '/components/student/video/student.html',
-	providers: [Session, StudentService],
-	directives: [ROUTER_DIRECTIVES],
+    selector: 'student-video',
+    templateUrl: '/components/student/video/student.html',
+    providers: [Session, StudentService],
+    directives: [ROUTER_DIRECTIVES],
 })
-export class StudentVideo implements AfterViewInit{
+export class StudentVideo implements AfterViewInit {
 
-	@Input() content: any;
-	@Input() lessonname: string;
-	@Input() index: number;
-	@Input() total: number;
+    @Input() content: any;
+    @Input() lessonname: string;
+    @Input() index: number;
+    @Input() total: number;
 
-	@Output() gotoNextContent=new EventEmitter();
-	@Output() gotoPreviousContent=new EventEmitter();
-	
-	htmlString: string = '';
+    @Output() gotoNextContent = new EventEmitter();
+    @Output() gotoPreviousContent = new EventEmitter();
 
-	constructor(private _session: Session, private _studentService: StudentService, private _router: Router) {
-		
-	}
+    htmlString: string = '';
 
-	ngOnInit() {
-		console.log(this.content);
-		this.htmlString = this.content.slideContent;
-		document.getElementById('iframe_id')['contentWindow'].document.write(`
-		<html><head><style id="mceDefaultStyles" type="text/css">.mce-content-body div.mce-resizehandle {position: absolute;border: 1px solid black;box-sizing: box-sizing;background: #FFF;width: 7px;height: 7px;z-index: 10000}.mce-content-body .mce-resizehandle:hover {background: #000}.mce-content-body img[data-mce-selected],.mce-content-body hr[data-mce-selected] {outline: 1px solid black;resize: none}.mce-content-body .mce-clonedresizable {position: absolute;outline: 1px dashed black;opacity: .5;filter: alpha(opacity=50);z-index: 10000}.mce-content-body .mce-resize-helper {background: #555;background: rgba(0,0,0,0.75);border-radius: 3px;border: 1px;color: white;display: none;font-family: sans-serif;font-size: 12px;white-space: nowrap;line-height: 14px;margin: 5px 10px;padding: 5px;position: absolute;z-index: 10001}
+    constructor(private _session: Session, private _studentService: StudentService, private _router: Router) {
+
+    }
+
+    ngOnInit() {
+        console.log(this.content);
+        setInterval(() => {
+            if (this.htmlString != this.content.slideContent) {
+                this.writeContent();
+            }
+        }, 200);
+    }
+
+    ngAfterViewInit() {
+        console.log(document.getElementById('iframe_id'));
+    }
+
+    writeContent() {
+        this.htmlString = this.content.slideContent;
+				document.getElementById('iframe_id')['contentWindow'].document.open();
+        document.getElementById('iframe_id')['contentWindow'].document.write(`
+<html><head><style id="mceDefaultStyles" type="text/css">.mce-content-body div.mce-resizehandle {position: absolute;border: 1px solid black;box-sizing: box-sizing;background: #FFF;width: 7px;height: 7px;z-index: 10000}.mce-content-body .mce-resizehandle:hover {background: #000}.mce-content-body img[data-mce-selected],.mce-content-body hr[data-mce-selected] {outline: 1px solid black;resize: none}.mce-content-body .mce-clonedresizable {position: absolute;outline: 1px dashed black;opacity: .5;filter: alpha(opacity=50);z-index: 10000}.mce-content-body .mce-resize-helper {background: #555;background: rgba(0,0,0,0.75);border-radius: 3px;border: 1px;color: white;display: none;font-family: sans-serif;font-size: 12px;white-space: nowrap;line-height: 14px;margin: 5px 10px;padding: 5px;position: absolute;z-index: 10001}
 .mce-visual-caret {position: absolute;background-color: black;background-color: currentcolor;}.mce-visual-caret-hidden {display: none;}*[data-mce-caret] {position: absolute;left: -1000px;right: auto;top: 0;margin: 0;padding: 0;}
 .mce-content-body .mce-offscreen-selection {position: absolute;left: -9999999999px;max-width: 1000000px;}.mce-content-body *[contentEditable=false] {cursor: default;}.mce-content-body *[contentEditable=true] {cursor: text;}
 </style><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link type="text/css" rel="stylesheet" href="https://cdn.tinymce.com/4/skins/lightgray/content.min.css"><link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:300,300i,400,400i"><link type="text/css" rel="stylesheet" href="//www.tinymce.com/css/codepen.min.css"><link rel="stylesheet" data-mce-href="https://cdn.tinymce.com/4/plugins/codesample/css/prism.css" href="https://cdn.tinymce.com/4/plugins/codesample/css/prism.css"></head><body id="tinymce" class="mce-content-body " data-id="tinymceN1481899178641" contenteditable="true" spellcheck="false">${this.htmlString}</body></html>`);
-	}
-	
-	ngAfterViewInit() {
-		console.log(document.getElementById('iframe_id'));
-	}
-	
-	gotoNext(){
-		this.gotoNextContent.emit({});
-	}
-	gotoPrevious(){
-		this.gotoPreviousContent.emit({});
-	}
-	getEmbedUrl(){
-		let videoNumber = this.content.videoEmbedCode.replace(/\D/g, '');
-		if(this.content.videoEmbedCode.includes('youtube')){
-			return this.content.videoEmbedCode;
-		}else{
-			return 'https://player.vimeo.com/video/' + videoNumber;
-		}
-	}
-	getIndex(){
-		return this.index + 1;
-	}
+				document.getElementById('iframe_id')['contentWindow'].document.close();
+    }
+    gotoNext() {
+        console.log(this.content)
+        this.gotoNextContent.emit({});
+        console.log(this.content)
+    }
+    gotoPrevious() {
+        this.gotoPreviousContent.emit({});
+    }
+    getEmbedUrl() {
+        let videoNumber = this.content.videoEmbedCode.replace(/\D/g, '');
+        if (this.content.videoEmbedCode.includes('youtube')) {
+            return this.content.videoEmbedCode;
+        } else {
+            return 'https://player.vimeo.com/video/' + videoNumber;
+        }
+    }
+    getIndex() {
+        return this.index + 1;
+    }
 }
