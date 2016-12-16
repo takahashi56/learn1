f3645eec6ebf5f6eedf125d4270dc86501d332fc
chrisroad <chrisbrownapple001@gmail.com>
2016-12-16 09:57:51 -0500

    update

--- app/components/student/video/student.ts
+++ app/components/student/video/student.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, Output, EventEmitter} from 'angular2/core';
+import {Component, OnInit, Input, Output, EventEmitter, AfterViewInit} from 'angular2/core';
 import {Session} from '../../../services/session';
 import {ROUTER_DIRECTIVES, Router} from 'angular2/router';
 import {CanActivate} from 'angular2/router';
@@ -10,7 +10,7 @@ import {StudentService} from '../../../services/student';
 	providers: [Session, StudentService],
 	directives: [ROUTER_DIRECTIVES],
 })
-export class StudentVideo {
+export class StudentVideo implements AfterViewInit{
 
 	@Input() content: any;
 	@Input() lessonname: string;
@@ -29,6 +29,15 @@ export class StudentVideo {
 	ngOnInit() {
 		console.log(this.content);
 		this.htmlString = this.content.slideContent;
+		document.getElementById('iframe_id')['contentWindow'].document.write(`
+		<html><head><style id="mceDefaultStyles" type="text/css">.mce-content-body div.mce-resizehandle {position: absolute;border: 1px solid black;box-sizing: box-sizing;background: #FFF;width: 7px;height: 7px;z-index: 10000}.mce-content-body .mce-resizehandle:hover {background: #000}.mce-content-body img[data-mce-selected],.mce-content-body hr[data-mce-selected] {outline: 1px solid black;resize: none}.mce-content-body .mce-clonedresizable {position: absolute;outline: 1px dashed black;opacity: .5;filter: alpha(opacity=50);z-index: 10000}.mce-content-body .mce-resize-helper {background: #555;background: rgba(0,0,0,0.75);border-radius: 3px;border: 1px;color: white;display: none;font-family: sans-serif;font-size: 12px;white-space: nowrap;line-height: 14px;margin: 5px 10px;padding: 5px;position: absolute;z-index: 10001}
+.mce-visual-caret {position: absolute;background-color: black;background-color: currentcolor;}.mce-visual-caret-hidden {display: none;}*[data-mce-caret] {position: absolute;left: -1000px;right: auto;top: 0;margin: 0;padding: 0;}
+.mce-content-body .mce-offscreen-selection {position: absolute;left: -9999999999px;max-width: 1000000px;}.mce-content-body *[contentEditable=false] {cursor: default;}.mce-content-body *[contentEditable=true] {cursor: text;}
+</style><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><link type="text/css" rel="stylesheet" href="https://cdn.tinymce.com/4/skins/lightgray/content.min.css"><link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:300,300i,400,400i"><link type="text/css" rel="stylesheet" href="//www.tinymce.com/css/codepen.min.css"><link rel="stylesheet" data-mce-href="https://cdn.tinymce.com/4/plugins/codesample/css/prism.css" href="https://cdn.tinymce.com/4/plugins/codesample/css/prism.css"></head><body id="tinymce" class="mce-content-body " data-id="tinymceN1481899178641" contenteditable="true" spellcheck="false">${this.htmlString}</body></html>`);
+	}
+	
+	ngAfterViewInit() {
+		console.log(document.getElementById('iframe_id'));
 	}
 	
 	gotoNext(){
